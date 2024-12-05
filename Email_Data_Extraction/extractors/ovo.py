eae9d7d226c0ccf78d9221b3c05f92594a1e08d1
Darius Mulyadi Putra <118329342+Duarss@users.noreply.github.com>
2024-12-05 14:20:02 +0700

    Updated ovo, bri, and ocbc extractors

--- Email_Data_Extraction/extractors/ovo.py
+++ Email_Data_Extraction/extractors/ovo.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from .base_extractor import BaseExtractor, EmailContent, TransactionData
+from .base_extractor import BaseExtractor, TransactionData
 import re
 import datetime
 
@@ -7,9 +7,8 @@ class OVOExtractor(BaseExtractor):
     def match(self, title: str, email_from: str) -> bool:
         return title == "OVO QR Payment Receipt" and email_from == "noreply@ovo.co.id"
     
-    def extract(self, content: EmailContent) -> list[TransactionData]:
+    def extract(self, content: str) -> list[TransactionData]:
         # Extract payment details from OVO email content
-        email = content.get_plaintext()
 
         trx = TransactionData()
 
@@ -18,28 +17,33 @@ class OVOExtractor(BaseExtractor):
         trx.payment_method = "OVO"
 
         # Extracting Amount and Currency
-        amount_match = re.search(r"Pembayaran\s+Rp([\d,\.]+)", email)
+        amount_match = re.search(r"Pembayaran\s*-\s*Rp([\d,\.]+)", content)
         if amount_match:
             trx.currency = "IDR"
             trx.amount = Decimal(amount_match.group(1).replace(",", "").replace(".", ""))
 
         # Extract merchant name
-        trx.merchant = re.search(r"Nama Toko\s+(.+)", email).group(1)
+        merchant_match = re.search(r"Nama Toko\s*:\s*(.+)", content)
+        if merchant_match:
+            trx.merchant = merchant_match.group(1)
 
         # Extract transaction date
-        date_match = re.search(r"(\d{2} \w+ \d{4}, \d{2}:\d{2})", email)
+        date_match = re.search(r"(\d{2} \w+ \d{4}, \d{2}:\d{2})", content)
         if date_match:
             trx.date = datetime.datetime.strptime(date_match.group(1), "%d %b %Y, %H:%M")
 
         # Extract location (optional)
-        location_match = re.search(r"Lokasi\s+(.+)", email)
+        location_match = re.search(r"Lokasi\s*:\s*(.+)", content)
         if location_match:
             trx.description = f"Location: {location_match.group(1)}"
 
         # Additional details if needed
+        approval_code_match = re.search(r"Kode Approval\s*:\s*(.+)", content)
+        payment_method_match = re.search(r"Metode Pembayaran\s*:\s*(.+)", content)
+
         trx.extra_data = {
-            "Approval Code": re.search(r"Kode Approval\s+(.+)", email).group(1),
-            "Payment Method": re.search(r"Metode Pembayaran\s+(.+)", email).group(1),
+            "Approval Code": approval_code_match.group(1) if approval_code_match else None,
+            "Payment Method": payment_method_match.group(1) if payment_method_match else None,
         }
 
         return [trx]
