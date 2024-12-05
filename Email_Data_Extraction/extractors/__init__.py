15f63ca8ad69977d3cf2ac9ace31b5fa3b0217b8
Nathan Adhitya <NathanAdhitya@users.noreply.github.com>
2024-12-05 21:17:01 +0700

    fix errors

--- Email_Data_Extraction/extractors/__init__.py
+++ Email_Data_Extraction/extractors/__init__.py
@@ -1,9 +1,9 @@
 from .base_extractor import BaseExtractor, TransactionData
-from .bri import BriExtractor
+from .bri import BRIExtractor
 from .jago import JagoExtractor
 from .livin import MandiriExtractor
 from .mybca import MyBCAExtrator
-from .ocbc import OcbcExtractor
+from .ocbc import OCBCExtractor
 from .ovo import OVOExtractor
 from .seabank import SeaBankExtractor
 from .grab import GrabExtractor
