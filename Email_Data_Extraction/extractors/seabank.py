fa5395ae94cfb7249accc91c3eb2e13310028d8d
Fransiscus Xaverius Surya Darmawan <s160422003@student.ubaya.ac.id>
2024-12-06 11:30:24 +0700

    Update seabank.py

--- Email_Data_Extraction/extractors/seabank.py
+++ Email_Data_Extraction/extractors/seabank.py
@@ -6,10 +6,68 @@ import datetime
 
 class SeaBankExtractor(BaseExtractor):
     def match(self, title: str, email_from: str) -> bool:
-        return "Notifikasi Transfer SeaBank" in title.lower() and email_from == "seabank@seabank.com"
+        valid_titles = [
+            "notifikasi transaksi seabank",
+            "notifikasi transfer seabank"
+        ]
+        valid_emails = [
+            "alerts@seabank.co.id"
+            # "m320b4ky1551@bangkit.academy"
+        ]
+        is_title_valid = any(valid_title in title.lower() for valid_title in valid_titles)
+        is_email_valid = any(valid_email in email_from.lower() for valid_email in valid_emails)
 
-    def extract(self, content: EmailContent) -> list[TransactionData]:
-        email = content.get_plaintext()
+        return is_title_valid and is_email_valid
+
+    #     return (
+    #     "notifikasi transaksi seabank" in title.lower() and
+    #     "alerts@seabank.co.id" in email_from.lower()
+    # )
+
+    # alerts@seabank.co.id
+    def extract_instant_payment_transaction(self, content: EmailContent) -> list[TransactionData]:
+        email = content
+        # Example format:
+        # Hai FX. SURYA DARMAWAN,
+        # Terima kasih telah menggunakan layanan SeaBank. Permintaan pembayaran kamu berhasil diproses. Berikut ini adalah detail transaksi kamu:
+        # Waktu Transaksi: 30 Nov 2024 11:11
+        # Jenis Transaksi: SeaBank Bayar Instan
+        # Transfer Dari: FX. SURYA DARMAWAN - XXXXXXXXX6749
+        # Nama Merchant: Shopee
+        # Username: fransxeagle
+        # Jumlah: Rp38.000
+        # Biaya: Rp0
+        # No. Referensi: 20241130435053419703
+        # Catatan: Note
+
+        trx = TransactionData()
+        trx.is_incoming = False 
+        trx.payment_method = "SeaBank" 
+
+        # Split per line and match
+        match = re.search(r"Jumlah\s*[\r\n]+\s*Rp([\d,\.]+)", email)
+
+        amount = match.group(1)
+        trx.currency = "IDR"
+        trx.amount = Decimal(amount.replace(".", ""))
+
+        match = re.search(r"Catatan\s*[\r\n]+\s*(\S+)", email)
+        trx.description = match.group(1).strip() 
+        match = re.search(r"Nama Merchant\s*[\r\n]+\s*(\S+)", email)
+        trx.merchant = match.group(1)
+
+        match = re.search(r"No\. Referensi\s*[\r\n]+\s*(\d+)", email)
+        trx.trx_id = match.group(1)
+
+        match = re.search(r"Waktu Transaksi\s*[\r\n]+\s*(\d{2} \w{3} \d{4} \d{2}:\d{2})", email)
+        date_str = match.group(1) 
+        trx.date = datetime.datetime.strptime(date_str, "%d %b %Y %H:%M")
+
+        return [trx]
+    
+
+    def extract_transfer_transaction(self, content: EmailContent) -> list[TransactionData]:
+        email = content
         # Example format:
         # Waktu Transaksi : 22 Nov 2024 18:48
         # Jenis Transaksi : Real Time (BI-FAST)
@@ -23,14 +81,33 @@ class SeaBankExtractor(BaseExtractor):
         trx.payment_method = "SeaBank" 
 
         # Split per line and match
-        amount = re.search(r"Jumlah\s*:\s*Rp([\d,\.]+)", email).group(1)
-        trx.currency = "IDR"
-        trx.amount = Decimal(amount.replace(",", ""))
+        match = re.search(r"Jumlah\s*[\r\n]+\s*Rp([\d,\.]+)", email)
 
-        trx.description = "Seabank Transaction"  # Gak ada deskripsi di bank Seabank
-        trx.merchant = re.search(r"Rekening Tujuan\s*:\s*(.+)", email).group(1)
-        trx.trx_id = re.search(r"No\. Referensi\s*:\s*(\d+)", email).group(1)
-        date_str = re.search(r"Waktu Transaksi\s*:\s*(\d{2} \w{3} \d{4} \d{2}:\d{2})", email).group(1)
+        amount = match.group(1)
+        trx.currency = "IDR"
+        trx.amount = Decimal(amount.replace(".", ""))
+
+        match = re.search(r"Catatan\s*[\r\n]+\s*(\S+)", email)
+        if match:
+            trx.description = match.group(1).strip() 
+        else:
+            trx.description = ""
+        match = re.search(r"Rekening Tujuan\s*[\r\n]+\s*(\S+)", email)
+        trx.merchant = match.group(1)
+
+        match = re.search(r"No\. Referensi\s*[\r\n]+\s*(\d+)", email)
+        trx.trx_id = match.group(1)
+
+        match = re.search(r"Waktu Transaksi\s*[\r\n]+\s*(\d{2} \w{3} \d{4} \d{2}:\d{2})", email)
+        date_str = match.group(1) 
         trx.date = datetime.datetime.strptime(date_str, "%d %b %Y %H:%M")
 
         return [trx]
+    
+    def extract(self, content: EmailContent) -> list[TransactionData]:
+        email = content.get_plaintext()
+        if "SeaBank Bayar Instan" in email:
+            return self.extract_instant_payment_transaction(email)
+        elif "Real Time (BI-FAST)" in email:
+            return self.extract_transfer_transaction(email)
+        return []
