7c07f1bd6cacc42d3c91b18f131d1f2fd3291b43
Duarss <s160422035@student.ubaya.ac.id>
2024-12-13 13:53:57 +0700

    fix categrization_model.py

--- Training_Model/categorization_model.py
+++ Training_Model/categorization_model.py
@@ -5,16 +5,13 @@ import random
 import nltk
 from nltk.corpus import wordnet
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, BatchNormalization, LeakyReLU, Dropout, CategoryEncoding, TextVectorization
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import regularizers
+import keras
+from keras.optimizers import Adam
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 from datetime import datetime
 from decimal import Decimal
-from tensorflow.keras.layers import IntegerLookup
-from tensorflow.keras.layers import StringLookup
 #from ..Email_Data_Extraction.extractors.base_extractor import TransactionData
 
 # Ensure nltk WordNet is downloaded
@@ -76,12 +73,12 @@ def preprocess_transaction_data(train_df, test_df):
     train_df['Merchant Name'] = train_df['Merchant Name'].apply(lambda x: augment_text_with_synonyms(x, num_replacements=1))
 
     # TF-IDF for 'Notes' and 'Merchant Name'
-    tfidf_vectorizer_notes = TextVectorization(max_tokens=150, output_mode='tf-idf')
+    tfidf_vectorizer_notes = keras.layers.TextVectorization(max_tokens=150, output_mode='tf-idf')
     tfidf_vectorizer_notes.adapt(tf.constant(train_df['Notes'].values, dtype=tf.string))
     tfidf_train_notes = tfidf_vectorizer_notes(tf.constant(train_df['Notes'].values, dtype=tf.string)).numpy().astype(np.float32)
     tfidf_test_notes = tfidf_vectorizer_notes(tf.constant(test_df['Notes'].values, dtype=tf.string)).numpy().astype(np.float32)
 
-    tfidf_vectorizer_merchant = TextVectorization(max_tokens=150, output_mode='tf-idf')
+    tfidf_vectorizer_merchant = keras.layers.TextVectorization(max_tokens=150, output_mode='tf-idf')
     tfidf_vectorizer_merchant.adapt(tf.constant(train_df['Merchant Name'].values, dtype=tf.string))
     tfidf_train_merchant = tfidf_vectorizer_merchant(tf.constant(train_df['Merchant Name'].values, dtype=tf.string)).numpy().astype(np.float32)
     tfidf_test_merchant = tfidf_vectorizer_merchant(tf.constant(test_df['Merchant Name'].values, dtype=tf.string)).numpy().astype(np.float32)
@@ -96,8 +93,8 @@ def preprocess_transaction_data(train_df, test_df):
     test_df['Transaction Type'] = test_df['Transaction Type'].astype(str)
 
     # String Lookup for categorical columns
-    lookup_payment_method = tf.keras.layers.StringLookup()
-    lookup_transaction_type = tf.keras.layers.StringLookup()
+    lookup_payment_method = keras.layers.StringLookup()
+    lookup_transaction_type = keras.layers.StringLookup()
 
     # Adapt lookup layers
     lookup_payment_method.adapt(tf.constant(train_df['Payment Method'], dtype=tf.string))
@@ -139,24 +136,24 @@ def preprocess_transaction_data(train_df, test_df):
 
 # Create a complex model for the task
 def create_model(input_dim, output_dim, category_encoding_layer, tfidf_vectorizer_notes, tfidf_vectorizer_merchant):
-    model = Sequential([
-        Dense(1024, kernel_regularizer=regularizers.l1_l2(l1=1e-8, l2=1e-7)),
-        BatchNormalization(momentum=0.9),
-        LeakyReLU(alpha=0.1),
-        Dropout(0.1),
-        Dense(512, kernel_regularizer=regularizers.l1_l2(l1=1e-8, l2=1e-7)),
-        BatchNormalization(momentum=0.9),
-        LeakyReLU(alpha=0.1),
-        Dropout(0.1),
-        Dense(256, kernel_regularizer=regularizers.l1_l2(l1=1e-8, l2=1e-7)),
-        BatchNormalization(momentum=0.9),
-        LeakyReLU(alpha=0.1),
-        Dropout(0.1),
-        Dense(128, kernel_regularizer=regularizers.l1_l2(l1=1e-8, l2=1e-7)),
-        BatchNormalization(momentum=0.9),
-        LeakyReLU(alpha=0.1),
-        Dropout(0.1),
-        Dense(output_dim, activation='softmax')
+    model = keras.models.Sequential([
+        keras.layers.Dense(1024, kernel_regularizer=keras.regularizers.l1_l2(l1=1e-8, l2=1e-7)),
+        keras.layers.BatchNormalization(momentum=0.9),
+        keras.layers.LeakyReLU(negative_slope=0.1),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(512, kernel_regularizer=keras.regularizers.l1_l2(l1=1e-8, l2=1e-7)),
+        keras.layers.BatchNormalization(momentum=0.9),
+        keras.layers.LeakyReLU(negative_slope=0.1),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(256, kernel_regularizer=keras.regularizers.l1_l2(l1=1e-8, l2=1e-7)),
+        keras.layers.BatchNormalization(momentum=0.9),
+        keras.layers.LeakyReLU(alpha=0.1),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(128, kernel_regularizer=keras.regularizers.l1_l2(l1=1e-8, l2=1e-7)),
+        keras.layers.BatchNormalization(momentum=0.9),
+        keras.layers.LeakyReLU(alpha=0.1),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(output_dim, activation='softmax')
     ])
     return model
 
@@ -187,29 +184,31 @@ def train_and_evaluate(training_data_path, testing_data_path):
 
     # Encode the target labels for training data
     le_category = LabelEncoder()
-    y_train = le_category.fit_transform(train_df['Category'])
+    y_train = np.array(le_category.fit_transform(train_df['Category']))
     print("y_train Shape:", y_train.shape)
 
     # If `Category` in the test data is NaN, y_test will be empty
     y_test = le_category.transform(test_df['Category'].dropna()) if not test_df['Category'].isna().all() else np.array([])
-    print("y_test Shape:", y_test.shape)
+    if isinstance(y_test, np.ndarray):
+        print("y_test Shape:", y_test.shape)
+    else:
+        print("y_test is not an array.")
 
     save_pickle(le_category, 'trained/label_encoder.pkl')
 
     # One-hot encode the training labels
-    y_train_onehot = tf.keras.utils.to_categorical(y_train)
+    y_train_onehot = keras.utils.to_categorical(y_train)
     print("y_train_onehot Shape:", y_train_onehot.shape)
 
     # One-hot encode the test labels if they exist
-    y_test_onehot = tf.keras.utils.to_categorical(y_test) if y_test.size > 0 else None
+    y_test_onehot = keras.utils.to_categorical(y_test) if y_test.size > 0 else None
     if y_test_onehot is not None:
         print("y_test_onehot Shape:", y_test_onehot.shape)
 
     # Create the model
     model = create_model(X_train.shape[1], y_train_onehot.shape[1], lookup_payment_method, tfidf_vectorizer_notes, tfidf_vectorizer_merchant)
     
-    # Compile the model
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
     # Train the model
     history = model.fit(X_train, y_train_onehot, epochs=15, batch_size=64, validation_split=0.2, verbose=1)
